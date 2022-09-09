310a674914ba6094589a45febb77ff600a0fe6b8
Olga <princessca.olga@icloud.com>
2022-09-09 11:25:31 +0300

    all

---
+++ Palindrom/n3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Введите число: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+ for (int i=1; 1<=10; i++)
+ {
+    for (int j =1; j <= N; j++)
+    {
+        Console.WriteLine($"{Math.Pow(j,3)}, "); 
+    }
+   
+     Console.WriteLine(" ");
+ break;
+  }   
+
+
+
+
