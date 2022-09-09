cce1a751868049faa357fae401af588f9215abe6
Olga <princessca.olga@icloud.com>
2022-09-09 11:21:22 +0300

    new

---
+++ Palindrom/XYZ/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введите координаты х1 =");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите кординаты x2 =");
+int x2=int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y1 =");
+int y1= int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y2 =");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты z1 =");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты z2 =");
+int z2 = int.Parse(Console.ReadLine());
+
+double l = Math.Sqrt((Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)));
+ Console.WriteLine($"l = {l}");
